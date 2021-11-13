167f03283cfe309554e592bbf8961a3be48f6c7c
nanirama <nani.rama1982@gmail.com>
2021-11-14 05:17:04 +0530

    changes 3

--- src/components/BankingBannersection.js
+++ src/components/BankingBannersection.js
@@ -2,7 +2,8 @@ import * as React from "react"
 import styled from "styled-components";
 import { graphql, useStaticQuery } from 'gatsby'
 import BackgroundImage from 'gatsby-background-image'
-import bgImg from "../assets/images/banking-banner.png";
+import ReactPlayer from 'react-player'
+import video from "../assets/videos/video1.mp4";
 
 const BankingBanner = () => {
   const data = useStaticQuery(
@@ -22,15 +23,29 @@ const BankingBanner = () => {
   return(
   <Wrapper>
      <Container>
-     <BackgroundImage
-         Tag="section"
-         fluid={imageData}
-      >
         <Banner>
-           <Heading>Private Banking no longer just for the<br /> super rich</Heading>
+        <ReactPlayer
+            className='react-player fixed-bottom'
+            url= {video}
+            width='100%'
+            height='100%'
+            loop={true}
+            controls = {true}
+
+            />
+           {/* <Heading>Private Banking no longer just for the<br /> super rich</Heading>
            <Paragraph>Otto Von Bismarck influence has shaped the financial industry for over 200 years. Now, we’re<br/> bringing it to the 21st century and giving you the opportunity to join the Count Bismarck famil</Paragraph>
+           <VideoText>
+           <AnchorLink href='#'>
+             <SvgIcon>
+             <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
+</svg>
+</SvgIcon>
+ Video Content
+ </AnchorLink>
+ </VideoText> */}
         </Banner>
-      </BackgroundImage>
      </Container>
   </Wrapper>
   )
@@ -45,7 +60,7 @@ position: relative;
     top: 0%;
     left: 0;
     width: 100%;
-    height:430px;
+    height: 430px;
     background-color: #202252;
     z-index: -1;
   }
@@ -56,18 +71,19 @@ margin: 0 auto;
 padding: 0 15px;
 `;
 const Banner = styled.div`
-height: 500px;
-margin-top: -80px;
+height: 680px;
+margin-top: -175px;
 display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
 text-align: center;
-padding: 0 15px;
+padding: 0px;
+position: relative;
 `;
 const Heading = styled.h2`
 color: #fff;
-margin:0;
+margin: 0;
 line-height: 56px;
   @media only screen and (max-width:991px){
     line-height: 45px;
@@ -78,4 +94,21 @@ line-height: 56px;
 `;
 const Paragraph = styled.p`
 color: #fff;
+`;
+const VideoText = styled.p`
+color: #fff;
+position: absolute;
+right: 20px;
+bottom: 20px;
+margin:0;
+  a{
+    color: #fff;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 15px;
+  }
+`;
+const SvgIcon = styled.span`
+padding: 2px 8px 0 0px;
+float: left;
 `;
