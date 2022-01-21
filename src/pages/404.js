23d343de962f409ce0efc0007714af40cf05c0bb
nanirama <nani.rama1982@gmail.com>
2022-01-21 07:36:55 +0530

    changes 61

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,14 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
+import GlobalStyle from '../layout/globalStyles';
+import FontStyles from "../layout/fontstyles";
 import Seo from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout page="404">
+     <GlobalStyle />
+     <FontStyles />
     <Seo title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
