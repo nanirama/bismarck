fe2e91a9c5ec851693988a24f8e1149e626ebdfe
nanirama <nani.rama1982@gmail.com>
2022-01-21 07:54:45 +0530

    changes 33

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-
+import styled from "styled-components";
 import Layout from "../components/layout"
 import GlobalStyle from '../layout/globalStyles';
 import FontStyles from "../layout/fontstyles";
@@ -10,9 +10,43 @@ const NotFoundPage = () => (
      <GlobalStyle />
      <FontStyles />
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Wrapper>
+      <Container>
+        <h1>404: Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Container>
+    </Wrapper>    
   </Layout>
 )
-
+const Wrapper = styled.div`
+padding: 70px 0;
+display: flex;
+background-color:#fff1e5;
+justify-content: center;
+align-items: center;
+text-align: center;
+   @media only screen and (max-width:1100px){
+      padding-bottom: 40px;
+   }
+    @media only screen and (max-width:991px){
+        padding: 40px 0;
+    }
+    @media only screen and (max-width:479px){
+        padding: 40px 0 30px 0;
+    }
+`;
+const Container = styled.div`
+max-width: 1170px;
+margin: 0 auto;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+@media only screen and (min-width:768px){
+  padding: 0 15px;
+}
+@media only screen and (max-width:767px){
+  padding: 0px;
+}
+`;
 export default NotFoundPage
